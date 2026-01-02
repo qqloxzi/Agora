dfb05aff2037b5b206d3528d8777585673c52c0f
qqloxzi <alikarakayagg@gmail.com>
2026-01-02 18:28:01 +0300

    dark theme, league participants

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // Koyu mod desteği için bu satır eklendi
   theme: {
     extend: {
       fontFamily: {
