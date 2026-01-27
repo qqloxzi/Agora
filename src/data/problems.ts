dc6afef46ee061d8c2bc5672247bf209ba3f13a7
qqloxzi <alikarakayagg@gmail.com>
2026-01-27 16:07:23 +0300

    go tree updates, sounds, and profile things.

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,10 +1,17 @@
+export interface SolutionNode {
+  x: number;
+  y: number;
+  color: string;
+  children?: SolutionNode[]; // Kendi tipinden çocukları olabilir
+  status?: 'correct' | 'wrong' | null;
+}
 export interface Problem {
   id: string;
   size: number;
   turn: 'black' | 'white'; 
   title: string;
   initialState: string;
-  solution: any[]; // Hem Array hem Tree yapısını destekler
+  solution: SolutionNode[] | { children: SolutionNode[] };
   category: string;
 }
 
@@ -22,72 +29,66 @@ export const problemSet: Problem[] = [
       "color": "white"
     }]
   },
- 
   {
-    id: "prob-4",
-    category: "Kurallar",
+    id: "prob-1",
+    category: "Açılış Prensipleri 1",
     size: 9,
     turn: "black",
-    title: "Beyaz Oynar (Dallanmış Çözüm)",
+    title: "Beyaz Oynar",
     initialState: '[[null,null,null,{\"x\":0,\"y\":3,\"color\":\"black\"},{\"x\":0,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":1,\"y\":0,\"color\":\"black\"},null,{\"x\":1,\"y\":2,\"color\":\"black\"},{\"x\":1,\"y\":3,\"color\":\"black\"},{\"x\":1,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":2,\"y\":1,\"color\":\"black\"},{\"x\":2,\"y\":2,\"color\":\"black\"},{\"x\":2,\"y\":3,\"color\":\"black\"},{\"x\":2,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":3,\"y\":0,\"color\":\"white\"},{\"x\":3,\"y\":1,\"color\":\"black\"},null,{\"x\":3,\"y\":3,\"color\":\"black\"},{\"x\":3,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":4,\"y\":1,\"color\":\"white\"},{\"x\":4,\"y\":2,\"color\":\"white\"},{\"x\":4,\"y\":3,\"color\":\"white\"},null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]',
-    // HATA DÜZELTİLDİ: Çift süslü parantez {{ }} kaldırıldı, tek { } yapıldı.
-    solution: {
-      "children": [
-        {
-          "x": 1,
-          "y": 2,
-          "color": "black",
-          "children": [
-            {
-              "x": 0,
-              "y": 2,
-              "color": "white",
-              "children": [
-                {
-                  "x": 1,
-                  "y": 3,
-                  "color": "black",
-                  "children": [
-                    {
-                      "x": 0,
-                      "y": 1,
-                      "color": "white",
-                      "children": [],
-                      "status": "correct"
-                    }
-                  ],
-                  "status": null
-                }
-              ],
-              "status": null
-            },
-            {
-              "x": 2,
-              "y": 2,
-              "color": "white",
-              "children": [
-                {
-                  "x": 2,
-                  "y": 1,
-                  "color": "black",
-                  "children": [],
-                  "status": "correct"
-                },
-                {
-                  "x": 7,
-                  "y": 2,
-                  "color": "black",
-                  "children": [],
-                  "status": "wrong"
-                }
-              ],
-              "status": null
-            }
-          ],
-          "status": null
-        }
-      ]
-    } as any // TypeScript'in Tree yapısına kızmaması için 'as any'
+    solution: [{
+      "x": 0,
+      "y": 1,
+      "color": "white"
+    }]
   },
-  
+  {
+    id: "prob-1",
+    category: "Açılış Prensipleri 1",
+    size: 9,
+    turn: "black",
+    title: "Beyaz Oynar",
+    initialState: '[[null,null,null,{\"x\":0,\"y\":3,\"color\":\"black\"},{\"x\":0,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":1,\"y\":0,\"color\":\"black\"},null,{\"x\":1,\"y\":2,\"color\":\"black\"},{\"x\":1,\"y\":3,\"color\":\"black\"},{\"x\":1,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":2,\"y\":1,\"color\":\"black\"},{\"x\":2,\"y\":2,\"color\":\"black\"},{\"x\":2,\"y\":3,\"color\":\"black\"},{\"x\":2,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":3,\"y\":0,\"color\":\"white\"},{\"x\":3,\"y\":1,\"color\":\"black\"},null,{\"x\":3,\"y\":3,\"color\":\"black\"},{\"x\":3,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":4,\"y\":1,\"color\":\"white\"},{\"x\":4,\"y\":2,\"color\":\"white\"},{\"x\":4,\"y\":3,\"color\":\"white\"},null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]',
+    solution: [{
+      "x": 0,
+      "y": 1,
+      "color": "white"
+    }]
+  },{
+    id: "prob-1",
+    category: "Açılış Prensipleri 2",
+    size: 9,
+    turn: "black",
+    title: "Beyaz Oynar",
+    initialState: '[[null,null,null,{\"x\":0,\"y\":3,\"color\":\"black\"},{\"x\":0,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":1,\"y\":0,\"color\":\"black\"},null,{\"x\":1,\"y\":2,\"color\":\"black\"},{\"x\":1,\"y\":3,\"color\":\"black\"},{\"x\":1,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":2,\"y\":1,\"color\":\"black\"},{\"x\":2,\"y\":2,\"color\":\"black\"},{\"x\":2,\"y\":3,\"color\":\"black\"},{\"x\":2,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":3,\"y\":0,\"color\":\"white\"},{\"x\":3,\"y\":1,\"color\":\"black\"},null,{\"x\":3,\"y\":3,\"color\":\"black\"},{\"x\":3,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":4,\"y\":1,\"color\":\"white\"},{\"x\":4,\"y\":2,\"color\":\"white\"},{\"x\":4,\"y\":3,\"color\":\"white\"},null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]',
+    solution: [{
+      "x": 0,
+      "y": 1,
+      "color": "white"
+    }]
+  },{
+    id: "prob-1",
+    category: "Açılış Prensipleri 2",
+    size: 9,
+    turn: "black",
+    title: "Beyaz Oynar",
+    initialState: '[[null,null,null,{\"x\":0,\"y\":3,\"color\":\"black\"},{\"x\":0,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":1,\"y\":0,\"color\":\"black\"},null,{\"x\":1,\"y\":2,\"color\":\"black\"},{\"x\":1,\"y\":3,\"color\":\"black\"},{\"x\":1,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":2,\"y\":1,\"color\":\"black\"},{\"x\":2,\"y\":2,\"color\":\"black\"},{\"x\":2,\"y\":3,\"color\":\"black\"},{\"x\":2,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":3,\"y\":0,\"color\":\"white\"},{\"x\":3,\"y\":1,\"color\":\"black\"},null,{\"x\":3,\"y\":3,\"color\":\"black\"},{\"x\":3,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":4,\"y\":1,\"color\":\"white\"},{\"x\":4,\"y\":2,\"color\":\"white\"},{\"x\":4,\"y\":3,\"color\":\"white\"},null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]',
+    solution: [{
+      "x": 0,
+      "y": 1,
+      "color": "white"
+    }]
+  },{
+    id: "prob-1",
+    category: "Ölüm & Kalım 1",
+    size: 9,
+    turn: "black",
+    title: "Beyaz Oynar",
+    initialState: '[[null,null,null,{\"x\":0,\"y\":3,\"color\":\"black\"},{\"x\":0,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":1,\"y\":0,\"color\":\"black\"},null,{\"x\":1,\"y\":2,\"color\":\"black\"},{\"x\":1,\"y\":3,\"color\":\"black\"},{\"x\":1,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":2,\"y\":1,\"color\":\"black\"},{\"x\":2,\"y\":2,\"color\":\"black\"},{\"x\":2,\"y\":3,\"color\":\"black\"},{\"x\":2,\"y\":4,\"color\":\"white\"},null,null,null,null],[{\"x\":3,\"y\":0,\"color\":\"white\"},{\"x\":3,\"y\":1,\"color\":\"black\"},null,{\"x\":3,\"y\":3,\"color\":\"black\"},{\"x\":3,\"y\":4,\"color\":\"white\"},null,null,null,null],[null,{\"x\":4,\"y\":1,\"color\":\"white\"},{\"x\":4,\"y\":2,\"color\":\"white\"},{\"x\":4,\"y\":3,\"color\":\"white\"},null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]',
+    solution: [{
+      "x": 0,
+      "y": 1,
+      "color": "white"
+    }]
+  },
 ];
