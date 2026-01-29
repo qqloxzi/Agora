9c387282fabe1d4de1195a55cda55ab1fd550892
qqloxzi <alikarakayagg@gmail.com>
2026-01-29 20:25:52 +0300

    problem json features for dev. and mobile configuraitons.

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -48,7 +48,86 @@ export const problemSet: Problem[] = [
     ]
   }
 },
+{
+  id: "3",
+  category: "Kurallar",
+  size: 9,
+  labels: "[[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,1,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,2,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]",
+  description: "Beyaz, 1 numaralı noktaya oynamak istediğinde taşın nefesi olmadığı için oraya oynayamaz. Fakat 2 numaralı noktaya oynamak istediğinde siyah taşları esir alıp kendine nefes açtığı için bu hamleyi yapabilir.",
+  turn: "white",
+  title: "Kurallar",
+  initialState: "[[null,null,null,null,null,null,null,null,null],[null,null,null,null,{\"color\":\"black\"},null,null,null,null],[null,null,null,{\"color\":\"black\"},null,{\"color\":\"black\"},null,null,null],[null,null,null,null,{\"color\":\"black\"},null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,{\"color\":\"white\"},{\"color\":\"white\"},null,null,null,null],[null,null,{\"color\":\"white\"},{\"color\":\"black\"},{\"color\":\"black\"},{\"color\":\"white\"},null,null,null],[null,null,{\"color\":\"black\"},null,{\"color\":\"white\"},{\"color\":\"black\"},null,null,null],[null,null,null,{\"color\":\"black\"},{\"color\":\"black\"},null,null,null,null]]",
+  solution: {
+    "children": [
+      {
+        "x": 7,
+        "y": 3,
+        "color": "white",
+        "children": [],
+        "status": "correct"
+      }
+    ]
+  }
+},
+{
+  id: "4",
+  category: "Kurallar",
+  size: 9,
+  labels: "[[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]",
+  description: "Beyaz, 1 numaralı noktaya oynamak istediğinde taşın nefesi olmadığı için oraya oynayamaz. Fakat 2 numaralı noktaya oynamak istediğinde siyah taşları esir alıp kendine nefes açtığı için bu hamleyi yapabilir.",
+  turn: "white",
+  title: "Kurallar",
+  initialState: "[[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[{\"color\":\"white\"},{\"color\":\"white\"},{\"color\":\"white\"},{\"color\":\"white\"},null,null,null,null,null],[{\"color\":\"black\"},{\"color\":\"black\"},{\"color\":\"black\"},{\"color\":\"white\"},null,null,null,null,null],[null,null,{\"color\":\"black\"},{\"color\":\"white\"},null,null,null,null,null],[{\"color\":\"black\"},{\"color\":\"black\"},{\"color\":\"black\"},{\"color\":\"white\"},null,null,null,null,null],[{\"color\":\"white\"},{\"color\":\"white\"},{\"color\":\"white\"},{\"color\":\"white\"},null,null,null,null,null],[null,null,null,null,null,null,null,null,null],[null,null,null,null,null,null,null,null,null]]",
+  solution: {
+    "children": [
+      {
+        "x": 4,
+        "y": 0,
+        "color": "white",
+        "children": [
+          {
+            "x": 4,
+            "y": 1,
+            "color": "black",
+            "children": [
+              {
+                "x": 4,
+                "y": 0,
+                "color": "white",
+                "children": [],
+                "status": "correct"
+              }
+            ],
+            "status": null
+          }
+        ],
+        "status": null
+      },
+      {
+        "x": 4,
+        "y": 1,
+        "color": "white",
+        "children": [
+          {
+            "x": 4,
+            "y": 0,
+            "color": "black",
+            "children": [
+              {
+                "x": 4,
+                "y": 1,
+                "color": "white",
+                "children": [],
+                "status": "correct"
+              }
+            ],
+            "status": null
+          }
+        ],
+        "status": null
+      }
+    ]
+  }
+},
 
-
- 
 ];
